3774fb1d775a865786c6619087cd60794c9fa2f8
yangzhao <yangzhao28@gmail.com>
2016-01-26 11:08:52 +0800

    update

--- rotationfile/rotationfile.go
+++ rotationfile/rotationfile.go
@@ -1,8 +1,11 @@
 package rotationfile
 
 import (
+	// "fmt"
+	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -12,10 +15,11 @@ type Rotator struct {
 	internalFile     *os.File
 	rotationByTime   int
 	nextRotationTime int64
+	fileLock         sync.Mutex
 }
 
-func (rotator *Rotator) GetCurrentFileName() string {
-	return rotator.currentFileName
+func (this *Rotator) GetCurrentFileName() string {
+	return this.currentFileName
 }
 
 const (
@@ -40,60 +44,84 @@ func GetTimeFormat(rotationByTime int) string {
 	}
 }
 
-func (rotator *Rotator) Create(name string, rotationByTime int) {
-	if name[len(name)-1] == '\\' || name[len(name)-1] == '/' {
-		rotator.baseFileName = name + "default.log"
-	} else {
-		rotator.baseFileName = name
+func (this *Rotator) createSymLink(currentName string) error {
+	fmt.Println("enter symlink")
+	linkName := this.baseFileName
+	if info, err := os.Lstat(linkName); !os.IsNotExist(err) {
+		if info != nil && (info.Mode()&os.ModeSymlink == os.ModeSymlink) {
+			// remove old link file
+			os.Remove(linkName)
+		} else {
+			// link exist but not symlink, use some alter name as linkname
+			linkName += ".alt"
+			return nil
+		}
 	}
-	if strings.LastIndexAny(name, "\\/") != -1 {
-		os.MkdirAll(name[0:strings.LastIndexAny(name, "\\/")], 0766)
+	if info, err := os.Lstat(currentName); err != nil {
+		if !info.Mode().IsRegular() {
+			os.Symlink(currentName, linkName)
+		}
 	}
-	// fmt.Println("name:", name)
-	rotator.rotationByTime = rotationByTime
-	now := time.Now()
-	rotator.switchFile(now)
+	return nil
 }
 
-func (rotator *Rotator) switchFile(now time.Time) error {
-	if rotator.rotationByTime != NoRotation {
-		logFileName := rotator.baseFileName
-		logFileName += "." + now.Format(GetTimeFormat(rotator.rotationByTime))
+func (this *Rotator) switchFile(now time.Time) error {
+	if this.rotationByTime != NoRotation {
+		logFileName := this.baseFileName
+		logFileName += "." + now.Format(GetTimeFormat(this.rotationByTime))
 		// fmt.Println("next log-name will be", logFileName, ".")
-		switch rotator.rotationByTime {
+		switch this.rotationByTime {
 		default:
 			break
 		case MinutelyRotation:
-			rotator.nextRotationTime = now.Add(time.Minute).Add(-time.Duration(now.Second()) * time.Second).Unix()
+			this.nextRotationTime = now.Add(time.Minute).Add(-time.Duration(now.Second()) * time.Second).Unix()
 		case HourlyRotation:
-			rotator.nextRotationTime = now.Add(time.Hour).Add(-time.Duration(now.Second()+now.Minute()*60) * time.Second).Unix()
+			this.nextRotationTime = now.Add(time.Hour).Add(-time.Duration(now.Second()+now.Minute()*60) * time.Second).Unix()
 		case DailyRotation:
-			rotator.nextRotationTime = now.Add(24 * time.Hour).Add(-time.Duration(now.Hour()*3600+now.Minute()*60+now.Second()) * time.Second).Unix()
+			this.nextRotationTime = now.Add(24 * time.Hour).Add(-time.Duration(now.Hour()*3600+now.Minute()*60+now.Second()) * time.Second).Unix()
 		}
-		// fmt.Println("next rotation time-point will be", rotator.nextRotationTime, " vs now ", now.Unix(), ".")
+		// fmt.Println("next rotation time-point will be", this.nextRotationTime, " vs now ", now.Unix(), ".")
 		logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 		if err == nil {
-			rotator.currentFileName = logFileName
-			rotator.internalFile = logFile
+			this.currentFileName = logFileName
+			this.internalFile = logFile
+			this.createSymLink(logFileName)
 			// fmt.Println("file swapped.")
-		} else {
-			// fmt.Println(err.Error())
 		}
 		return err
 	}
 	return nil
 }
 
-func (rotator *Rotator) Write(p []byte) (n int, err error) {
-	now := time.Now()
-	if now.Unix() >= rotator.nextRotationTime {
-		if err := rotator.switchFile(now); err != nil {
-			return 0, err
-		}
+func (this *Rotator) Create(name string, rotationByTime int) {
+	if name[len(name)-1] == '\\' || name[len(name)-1] == '/' {
+		this.baseFileName = name + "default.log"
+	} else {
+		this.baseFileName = name
 	}
-	return rotator.internalFile.Write(p)
+	if strings.LastIndexAny(name, "\\/") != -1 {
+		os.MkdirAll(name[0:strings.LastIndexAny(name, "\\/")], 0766)
+	}
+	// fmt.Println("name:", name)
+	this.rotationByTime = rotationByTime
+	now := time.Now()
+	this.switchFile(now)
 }
 
-func (rotator *Rotator) Close() {
-	rotator.internalFile.Close()
+func (this *Rotator) Write(p []byte) (n int, err error) {
+	now := time.Now()
+	if now.Unix() >= this.nextRotationTime {
+		this.fileLock.Lock()
+		defer this.fileLock.Unlock()
+		if now.Unix() >= this.nextRotationTime {
+			if err := this.switchFile(now); err != nil {
+				return 0, err
+			}
+		}
+	}
+	return this.internalFile.Write(p)
+}
+
+func (this *Rotator) Close() {
+	this.internalFile.Close()
 }
